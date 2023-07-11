37975afb1ed07d93a3afa2175663559a403a0284
jin-vita <jhj9144@gmail.com>
2023-07-11 10:54:20 +0900

    feat video player

--- app/src/main/java/com/jinvita/testvideoplayer/MainActivity.kt
+++ app/src/main/java/com/jinvita/testvideoplayer/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.jinvita.testvideoplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import androidx.appcompat.app.AppCompatActivity
+import com.jinvita.testvideoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val controller by lazy {
+        object : MediaController(this) {
+            override fun show() = super.show(0)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        with(binding) {
+            videoView.setMediaController(controller)
+            controller.setAnchorView(videoView)
+            playVideo(1)
+            button1.setOnClickListener { playVideo(1) }
+            button2.setOnClickListener { playVideo(2) }
+            button3.setOnClickListener { playVideo(3) }
+            button4.setOnClickListener { playVideo(4) }
+            button5.setOnClickListener { playVideo(5) }
+        }
+    }
+
+    private fun ActivityMainBinding.playVideo(index: Int) = with(videoView) {
+        setVideoPath(
+            "android.resource://$packageName/${
+                when (index) {
+                    1 -> R.raw.video1
+                    2 -> R.raw.video2
+                    3 -> R.raw.video3
+                    4 -> R.raw.video4
+                    5 -> R.raw.video5
+                    else -> R.raw.video1
+                }
+            }"
+        )
+        setOnCompletionListener {
+            controller.show(0)
+            start()
+        }
+        setOnPreparedListener { controller.show(0) }
+        start()
     }
 }
